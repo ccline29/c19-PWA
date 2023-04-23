16be3cdb2b8d794d59fe08105aa269d99cac6cc5
Chris Cline <115193125+ccline29@users.noreply.github.com>
2023-04-23 11:35:44 -0500

    all

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -4,47 +4,41 @@ const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+        console.log('JATE database already exists');
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      console.log('JATE database created');
     },
   });
 
-// Define an async function called putDb that takes a parameter called content
+
 export const putDb = async (content) => {
-  // Log an error message indicating that the function is not yet implemented
-  console.error('putDb not implemented')
-  // Create a new transaction with read-write access to the 'jate' object store
+  console.log('Put to the database');
+
+  const jateDb = await openDB('jate', 1);
   const txt = jateDb.transaction('jate', 'readwrite');
-  // Open the 'jate' database
-  const jateDb = await openDB('jate', 1);
-  // Get a reference to the 'jate' object store
   const store = txt.objectStore('jate');
-  const results = await request;
-  // Send a request to add the content to the database
-  const request = store.put({ jate: content });
-  // Log a message indicating that the content was saved
-  console.log('Saved!', results);
-};
+  const request = store.put({ id: 1, value: content });
+  console.log(request);
 
-
-// Define an async function called getDb
-export const getDb = async () =>{ 
-   // Log an error message indicating that the function is not yet implemented
-  console.error('getDb not implemented');
-   // Open the 'jate' database
-  const jateDb = await openDB('jate', 1);
-  // Create a new transaction with read-only access to the 'jate' object store
-  const txt = jateDb.transaction('jate', 'readonly');
-  // Get a reference to the 'jate' object store
-  const store = txt.objectStore('jate');
-  // Log the results of the request
-  const results = await request;
-  console.log('results.value', results);
-   // Send a request to get all the content from the database
-  const request = store.getAll();
+  const result = await request;
+  console.log('Data successfully saved to the database', result);
+  return result;
 }
-// Call the initdb function to create or upgrade the database
+
+
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const jateDb = await openDB('jate', 1);
+  const txt = jateDb.transaction('jate', 'readonly');
+  const store = txt.objectStore('jate');
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+}
+
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,31 +1,27 @@
-// Get a reference to the button element with the ID 'buttonInstall'
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// This event is fired when the browser is ready to prompt the user to install the PWA
-window.addEventListener('beforeinstallprompt', (event) => {
-    // Store the event object in a global variable so it can be accessed later
-    window.deferredPrompt = event;
-    // Make the install button visible by removing the 'hidden' class
-    butInstall.classList.toggle('hidden',false);
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  window.deferredPrompt = event;
+
+  butInstall.classList.toggle("hidden", false);
 });
 
-// Add a click event listener to the install button
-butInstall.addEventListener('click', async () => {
-    const promptEvent =window.deferredPrompt;
-    // If the prompt event does not exist, do nothing
-    if(!promptEvent){
-        return;
-    }
-    // Prompt the user to install the PWA
-    promptEvent.prompt();
-    window.deferredPrompt = null;
-    // Hide the install button by adding the 'hidden' class
-    butInstall.classList.toggle('hidden',true);
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
+
+  promptEvent.prompt();
+
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle("hidden", true);
 });
 
-// This event is fired when the PWA has been successfully installed
-window.addEventListener('appinstalled', (event) => {
-    // Clear the deferred prompt so it cannot be used again
-    window.deferredPrompt = null;
+window.addEventListener("appinstalled", (event) => {
+  window.deferredPrompt = null;
 });
+
